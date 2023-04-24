e7963d70b2a61c6215136b3c865df180d9eda2f5
Denys Morozov <ajax3101@gmail.com>
2023-04-24 22:29:39 +0300

    creating a structure cities

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import City
+from .forms import CityForm
 
-# Create your views here.
+
+def city_list(request):
+    cities = City.objects.all()
+    return render(request, 'cities/city_list.html', {'cities': cities})
+
+
+def city_create(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('city_list')
+    else:
+        form = CityForm()
+    return render(request, 'cities/city_create.html', {'form': form})
+
+
+def city_update(request, pk):
+    city = City.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = CityForm(request.POST, instance=city)
+        if form.is_valid():
+            form.save()
+            return redirect('city_list')
+    else:
+        form = CityForm(instance=city)
+    return render(request, 'cities/city_update.html', {'form': form})
+
+
+def city_delete(request, pk):
+    city = City.objects.get(pk=pk)
+    city.delete()
+    return redirect('city_list')
+
+def add_city(request):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = CityForm()
+    
+    return render(request, 'add_city.html', {'form': form})
