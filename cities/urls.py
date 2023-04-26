7174ec1cee7bc61c5e6449426528a79d78080125
Denys Morozov <ajax3101@gmail.com>
2023-04-26 20:39:45 +0300

    change model in trains

--- cities/urls.py
+++ cities/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.city_list, name='city_list'),
     path('add/', views.city_create, name='city_create'),
-    path('<int:pk>/', views.city_detail, name='city_detail'),
+   # path('<int:pk>/', views.city_detail, name='city_detail'),
     path('<int:pk>/edit/', views.city_update, name='city_update'),
     path('<int:pk>/delete/', views.city_delete, name='city_delete'),
 ]
--- trains/models.py
+++ trains/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+#from django.core.validators import MaxValueValidator 
+#from django.core.validators import RegexValidator
+
+
 
 class Train(models.Model):
-    train_number = models.IntegerField(max_length=10)
+    #train_number = models.IntegerField(max_length=10) https://stackoverflow.com/questions/30849862/django-max-length-for-integerfield
+    #train_number = models.IntegerField(validators=[MaxValueValidator(9999999999)]) 
+    # or train_number = models.CharField(max_length=10, validators=[RegexValidator(r'^\d{1,10}$')]
     begin_of_route = models.CharField(max_length=50)
     begin_of_route = models.CharField(max_length=50)
     travel_time = models.DurationField()
