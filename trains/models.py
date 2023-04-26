b41750122945f0ff590de84d99832532124d17a1
MaryTuryvna <120558314+MaryTuryvna@users.noreply.github.com>
2023-04-26 09:57:54 +0300

    Update models.py

--- trains/models.py
+++ trains/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Train(models.Model):
+    train_number = models.IntegerField(max_length=10)
+    begin_of_route = models.CharField(max_length=50)
+    begin_of_route = models.CharField(max_length=50)
+    travel_time = models.DurationField()
+
+    def __str__(self):
+        return self.train_number
