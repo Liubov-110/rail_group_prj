f601abfcee22eda2c73fdeff8556fed73b0ccb3e
Denys Morozov <ajax3101@gmail.com>
2023-05-01 23:36:23 +0300

    save_route add verbose

--- saved_routes/models.py
+++ saved_routes/models.py
@@ -3,10 +3,15 @@ from django.contrib.auth.models import User
 from routes.models import Route
 
 class SavedRoute(models.Model):
-    name = models.CharField(max_length=100)
-    route = models.ForeignKey(Route, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date_saved = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = 'Збережений маршрут'
+        verbose_name_plural = 'Збережені маршрути'
+        ordering = ['route']
+
+    name = models.CharField(max_length=100, verbose_name='Назва')
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, verbose_name='Маршрут')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Користувач')
+    date_saved = models.DateTimeField(auto_now_add=True, verbose_name='Дата збереження')
 
     def __str__(self):
         return self.name
